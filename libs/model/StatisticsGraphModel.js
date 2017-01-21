3b83832c5b34ed6b924ede03b659a34805a941a9
fuerbringer <severin@protonmail.ch>
2017-01-21 13:20:52 +0100

    Fixed graph issue

--- libs/model/StatisticsGraphModel.js
+++ libs/model/StatisticsGraphModel.js
@@ -3,7 +3,7 @@ var db = require(approot + '/libs/db');
 
 var StatisticsGraphModel = function() {
 	this.getData = function(callback) {
-		db.connection.execute('SELECT COUNT(*) count, timestamp FROM log_request GROUP BY DAY(timestamp)', function(err, results, fields) {
+		db.connection.execute('SELECT COUNT(*) count, timestamp FROM log_request WHERE YEAR(timestamp)=YEAR(CURDATE()) GROUP BY DAY(timestamp)', function(err, results, fields) {
 			if(err) {
 				console.log("SQL ERROR: " + err);
 			}
