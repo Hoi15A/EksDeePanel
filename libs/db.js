6f6e3b0e4b20bbc49c5ac09a6d4a7a3da1e20416
Austin <Hoi15A@users.noreply.github.com>
2017-01-29 18:15:30 +0100

    Switch to connection pool
    So the panel doesnt crash due to mysql timeout

--- libs/db.js
+++ libs/db.js
@@ -3,4 +3,8 @@ var jsonfile = require('jsonfile');
 var mysql = require('mysql2');
 
 var db_auth = jsonfile.readFileSync(approot + '/config/auth.json');
-exports.connection = mysql.createConnection(db_auth);
+exports.connection = mysql.createPool({
+    connectionLimit : 25, host: db_auth.host,
+		user: db_auth.user,
+		password: db_auth.password,
+database: db_auth.database});
